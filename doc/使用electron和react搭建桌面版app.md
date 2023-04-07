a9ed4401921f85c3d7f87e535af51086275f86ce
向意 <lwb320101@alibaba-inc.com>
2023-04-07 13:56:37 +0800

    add:初始化项目

---
+++ doc/使用electron和react搭建桌面版app.md
@@ -0,0 +1,1 @@
+# 使用electron和react搭建桌面app
---
+++ forge.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      platforms: ['darwin'],
+      config: {
+        overwrite: true,
+        name: "我的测试应用",
+        format: "ULFO"
+      }
+    }
+  ],
+};
